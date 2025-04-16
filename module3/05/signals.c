6fc87458c8c200604e7d6ed5b2a8de123f06c22a
mercy191 <ya.nikitaivanov.kz@gmail.com>
2025-04-17 02:48:11 +0700

    The magic constants have been changed and signal processing has been corrected

--- module3/05/signals.c
+++ module3/05/signals.c
@@ -9,7 +9,14 @@
 #include <signal.h>
 #include <time.h>
 
-volatile sig_atomic_t file_locked = 0;
+#define OUTPUT_FILE "output.txt"
+#define SLEEP_TIME  10000
+#define MSG_BUF     128
+
+#define LOCK        1
+#define UNLOCK      0
+
+volatile sig_atomic_t file_locked = UNLOCK;
 
 /* SIGUSR1 handle */
 void handle_sigusr1(int sig);
@@ -41,53 +48,43 @@ int main(int argc, char *argv[]) {
     
     int pipefd[2];
     if (pipe(pipefd) == -1) {
-        perror("pipe");
+        perror("pipe failed");
         exit(EXIT_FAILURE);
     }
     
     /* Create child process*/
     pid_t pid = fork();
     if (pid < 0) {
-        perror("fork");
+        perror("fork failed");
         exit(EXIT_FAILURE);
     }
     /* Child process */
     else if (pid == 0) {
         close(pipefd[0]);
-
-        if (child_process(pipefd[1], cycles) == EXIT_SUCCESS) {
-            close(pipefd[1]);
-            exit(EXIT_SUCCESS);
-        }
-        else {
-            close(pipefd[1]);
-            exit(EXIT_FAILURE);
-        }
+        int result = child_process(pipefd[1], cycles);
+        close(pipefd[1]);
+        
+        exit(result);
     }
     /* Parent process */
     else {
         close(pipefd[1]);
-
-        if (parent_process(pid, pipefd[0], cycles) == EXIT_SUCCESS) {
-            close(pipefd[0]);
-            exit(EXIT_SUCCESS);
-        }
-        else {
-            close(pipefd[0]);
-            exit(EXIT_SUCCESS);
-        }       
+        int result = parent_process(pid, pipefd[0], cycles);
+        close(pipefd[0]);
+        
+        exit(result);     
     }
        
 }
 
 /* SIGUSR1 handle */
 void handle_sigusr1(int sig) {
-    file_locked = 1;
+    file_locked = LOCK;
 }
 
 /* SIGUSER2 handle */
 void handle_sigusr2(int sig) {
-    file_locked = 0;
+    file_locked = UNLOCK;
 }
 
 void write_stdout(const char *str) {
@@ -97,39 +94,46 @@ void write_stdout(const char *str) {
 int child_process(int pipefd, int cycles) {       
     srand(time(NULL) ^ getpid());
         
+    signal(SIGUSR1, handle_sigusr1);
+    signal(SIGUSR2, handle_sigusr2);
+
     for (int i = 0; i < cycles; i++) {
-        /* SIGGUSR1 */
+        /* Wait SIGGUSR1 */
         while (file_locked) {
-            usleep(10000); 
+            usleep(SLEEP_TIME); 
         }
-            
-        int fd = open("output.txt", O_RDONLY);
+          
+        char msg[MSG_BUF];
+        int fd = open(OUTPUT_FILE, O_RDONLY);
         if (fd < 0) {
-            write_stdout("\nChild process: File 'output.txt' doesn't exists or empty.\n");
+            snprintf(msg, sizeof(msg), "\nChild process: File %s doesn't exists or empty.\n", OUTPUT_FILE);
+            write_stdout(msg);
         } 
         else {                             
-            char buffer[256];
+            snprintf(msg, sizeof(msg), "\nChild process: Read file %s:\n", OUTPUT_FILE);
+            write_stdout(msg);
+
+            char buffer[MSG_BUF];
             ssize_t bytesRead;
-            write_stdout("\nChild process: Read file 'output.txt':\n");
             while ((bytesRead = read(fd, buffer, sizeof(buffer))) > 0) {
                 if (write(STDOUT_FILENO, buffer, bytesRead) != bytesRead) {
-                    perror("write");
+                    perror("write failed");
                     break;
                 }
             }
             if (bytesRead < 0) {
-                perror("read");
+                perror("read failed");
             }
             close(fd);
         }
             
         int new_num = rand();
-        char msg[128];
+        
         snprintf(msg, sizeof(msg), "Child process: Send new number: %d\n", new_num);
         write_stdout(msg);
 
         if (write(pipefd, &new_num, sizeof(new_num)) != sizeof(new_num)) {
-            perror("pipe");
+            perror("pipe failed");
             return EXIT_FAILURE;           
         }
 
@@ -145,38 +149,39 @@ int parent_process(int child_pid, int pipefd, int cycles) {
         
         int received;
         if (read(pipefd, &received, sizeof(received)) != sizeof(received)) {
-            perror("pipe");
+            perror("pipe failed");
             return EXIT_FAILURE;
         }
         
         /* Send SIGUSR1 */
         if (kill(child_pid, SIGUSR1) == -1) {
-            perror("kill");
+            perror("kill failed");
         }
   
-        char msg[128];
+        char msg[MSG_BUF];
         snprintf(msg, sizeof(msg), "\nParent process: Received number: %d\n", received);
         write_stdout(msg);
         
         int fd = open(
-            "output.txt", 
+            OUTPUT_FILE, 
             O_WRONLY | O_CREAT | O_APPEND, 
             S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
         if (fd < 0) {
-            perror("open");
+            perror("open failed");
         } 
         else {
             snprintf(msg, sizeof(msg), "  Parent process: Number: %d\n", received);
             if (write(fd, msg, strlen(msg)) != (ssize_t)strlen(msg)) {
-                perror("write");
+                perror("write failed");
             }
             close(fd);
-            write_stdout("Parent process: Write number in file 'output.txt'\n");
+            snprintf(msg, sizeof(msg), "Parent process: Write number in file %s\n", OUTPUT_FILE);
+            write_stdout(msg);
         }
         
         /* Send SIGUSR2 */
         if (kill(child_pid, SIGUSR2) == -1) {
-            perror("kill");
+            perror("kill failed");
         }
         
         sleep(1);
