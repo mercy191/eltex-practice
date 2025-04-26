8ce5b1c766355982f9427cbe5d34201208a5ebfd
mercy191 <ya.nikitaivanov.kz@gmail.com>
2025-04-26 15:00:05 +0700

    Third version of multiplayer chat

--- module3/14/udp_client.c
+++ module3/14/udp_client.c
@@ -7,11 +7,17 @@
 #include <errno.h>
 #include <string.h>
 #include <signal.h>
+#include <pthread.h>
 
 #define MAX_BUF 1000
 
 volatile int        running = 1;
-
+int                 sockfd = -1;
+uint16_t            servport = 0;
+struct sockaddr_in  serveraddr;
+struct sockaddr_in  clientaddr;
+pthread_t           recvthread;
+pthread_t           sendthread;
 
 
 /* SIGINT handle, send stop message */
@@ -21,22 +27,17 @@ void handle_sigint();
 void write_stdout(const char *str);
 
 /* Listen socket */
-int listen_sock(int sockfd, struct sockaddr_in *addr, socklen_t *addrlen);
+void*  listen_thread(void *arg);
 
 /* Send message in socket */
-int send_sock(int sockfd, struct sockaddr_in *addr, socklen_t addrlen);
+void*  send_thread(void *arg);
 
 int main(int argc, char* argv[])
 {
     signal(SIGINT, handle_sigint);
 
-    int                 sockfd;
-    struct sockaddr_in  serveraddr;
-    struct sockaddr_in  clientaddr;
-    uint16_t            servport = 0;
-
-    if (argc != 3){
-        perror("Usage: udp_client <server IP address> <server port>\n");
+    if (argc != 3) {
+        fprintf(stderr, "Usage: udp_client <server IP address> <server port>\n");
         exit(EXIT_FAILURE);
     }
 
@@ -66,86 +67,76 @@ int main(int argc, char* argv[])
         close(sockfd);
         exit(EXIT_FAILURE);
     }
-    
-    pid_t pid = fork();
-    if (pid < 0) {
-        perror("fork failed");
+
+    if (pthread_create(&recvthread, NULL, listen_thread, NULL) != 0) {
+        perror("pthread_create failed");
         close(sockfd);
         exit(EXIT_FAILURE);
     }
-    /* Child process */
-    else if (pid == 0) {
-        int result = send_sock(sockfd, &serveraddr, (socklen_t) sizeof(serveraddr));
-        exit(result);
-    }
-    /* Parent process */
-    else {       
-        struct sockaddr_in recvaddr;
-        socklen_t recvaddrlen = sizeof(recvaddr);
-        int result = listen_sock(sockfd, &recvaddr, &recvaddrlen);
 
-        /* Send SIGINT */
-        if (kill(pid, SIGINT) == -1) {
-            perror("kill failed");
-            exit(EXIT_FAILURE);
-        }
-    
+    if (pthread_create(&sendthread, NULL, send_thread, NULL) != 0) {
+        perror("pthread_create failed");
+        running = 0;
+        pthread_cancel(recvthread); 
+        pthread_join(recvthread, NULL);
         close(sockfd);
-        exit(result);
+        exit(EXIT_FAILURE);
     }
+
+    pthread_join(recvthread, NULL);
+    pthread_join(sendthread, NULL);
+
+    close(sockfd);
+    exit(EXIT_SUCCESS);
 }
 
-void handle_sigint(int sockfd, struct sockaddr_in *addr) {
+void handle_sigint() {
     running = 0; 
-
-    struct sockaddr_in stopaddr;
-    stopaddr.sin_family = AF_INET;
-    stopaddr.sin_port = addr->sin_port;
-    stopaddr.sin_addr.s_addr = addr->sin_addr.s_addr;
-    sendto(sockfd, "exit", 4, 0, (struct sockaddr *) &stopaddr, (socklen_t) sizeof(stopaddr));
+    pthread_cancel(recvthread);
+    pthread_cancel(sendthread);  
 }
 
 void write_stdout(const char *str) {
     write(STDOUT_FILENO, str, strlen(str));
 }
 
-int listen_sock(int sockfd, struct sockaddr_in *addr, socklen_t *addrlen) {
-    
+void* listen_thread(void *arg) {
+    struct sockaddr_in recvaddr;
+    socklen_t recvaddrlen = sizeof(recvaddr);
     char bufline[MAX_BUF];  
 
     while (running) {  
+        pthread_testcancel();
+
         memset(bufline, 0, sizeof(bufline));
-        if (recvfrom(sockfd, bufline, MAX_BUF, 0, (struct sockaddr*) addr, addrlen) == -1) {
+        if (recvfrom(sockfd, bufline, MAX_BUF, 0, (struct sockaddr*) &recvaddr, &recvaddrlen) == -1) {
             perror("recvfrom failed");
-            close(sockfd);
-            return EXIT_FAILURE;
+            break;
         }
-    
-        if (!running) break;
     
         char msg[2*MAX_BUF];
         char ip_str[INET_ADDRSTRLEN];
-        inet_ntop(AF_INET, &addr->sin_addr, ip_str, INET_ADDRSTRLEN);
+        inet_ntop(AF_INET, &recvaddr.sin_addr, ip_str, INET_ADDRSTRLEN);
         snprintf(msg, sizeof(msg), "< (%s) %s", ip_str, bufline);
         write_stdout(msg);
     }
 
-    return EXIT_SUCCESS;
+    return NULL;
 }
 
-int send_sock(int sockfd, struct sockaddr_in *addr, socklen_t addrlen) {
+void* send_thread(void *arg) {
     char bufline[MAX_BUF];  
 
     while (running) {      
+        pthread_testcancel();
+
         memset(bufline, 0, sizeof(bufline));
         fgets(bufline, MAX_BUF, stdin);
-
-        if (sendto(sockfd, bufline, strlen(bufline) + 1, 0, (struct sockaddr*) addr, addrlen) == -1) {
+        if (sendto(sockfd, bufline, strlen(bufline) + 1, 0, (struct sockaddr*) &serveraddr, (socklen_t) sizeof(serveraddr)) == -1) {
             perror("sendto failed");
-            close(sockfd);
-            return EXIT_FAILURE;
+            break;
         }
     } 
-    return EXIT_SUCCESS;  
+    return NULL;  
 }
 
--- module3/14/udp_server.c
+++ module3/14/udp_server.c
@@ -7,6 +7,7 @@
 #include <errno.h>
 #include <string.h>
 #include <signal.h>
+#include <pthread.h>
 
 #define MAX_BUF     1000
 #define CLI_COUNT   3
@@ -16,7 +17,13 @@ struct socksaddr_in {
     int active;
 };
 
-volatile int running = 1;
+volatile int            running = 1;
+int                     client_count = 0;
+int                     sockfd = -1;
+uint16_t                servport = 0;
+struct sockaddr_in      serveraddr;
+struct socksaddr_in     clientsaddr[CLI_COUNT] = {0};
+pthread_t               procthread;
 
 /* SIGINT handle, send stop message */
 void handle_sigint();
@@ -34,25 +41,20 @@ int is_exists(struct socksaddr_in *clientsaddr, struct sockaddr_in *recvaddr);
 int add_clientaddr(struct socksaddr_in *clientsaddr, struct sockaddr_in *addr);
 
 /* Listen socket */
-int listen_sock(int sockfd, struct sockaddr_in *addr, socklen_t *addrlen);
+int listen_sock(int sockfd, struct sockaddr_in *addr, socklen_t *addrlen, char *bufline);
 
 /* Send message in socket */
-int send_sock(int sockfd, struct sockaddr_in *addr, socklen_t addrlen);
+int send_sock(int sockfd, struct sockaddr_in *addr, socklen_t addrlen, char *bufline);
+
+/* Deals with the processing and sending of messages */
+void* processing_thread(void *arg); 
 
 int main(int argc, char* argv[]) 
 {
-    signal(SIGINT, handle_sigint);
-     
-    int                     client_count = 0;
-    int                     sockfd;
-    struct sockaddr_in      serveraddr;
-    struct socksaddr_in     clientsaddr[CLI_COUNT] = {0};
-    struct sockaddr_in      recvaddr;
-    socklen_t               recvaddrlen = sizeof(recvaddr);
-    uint16_t                servport = 0;
+    signal(SIGINT, handle_sigint);  
 
     if (argc != 2) {
-        perror("Usage: udp_server <server port>\n");
+        fprintf(stderr, "Usage: udp_server <server port>\n");
         exit(EXIT_FAILURE);
     }
 
@@ -74,51 +76,21 @@ int main(int argc, char* argv[])
         exit(EXIT_FAILURE);
     }
 
-    /* Process one message at a time */
-    while (running) {
-        if (listen_sock(sockfd, &recvaddr, &recvaddrlen) == -1) {
-            close(sockfd);
-            exit(EXIT_FAILURE);
-        }
-
-        if (!running) break;
-
-        if (!is_exists(clientsaddr, &recvaddr) && client_count < CLI_COUNT) {             
-            int index = -1;
-            char msg[MAX_BUF];
-            if ((index = add_clientaddr(clientsaddr, &recvaddr)) != -1) {
-                client_count++;
-                snprintf(msg, sizeof(msg), "New client: %s:%d\n", inet_ntoa(clientsaddr[index].clientaddr.sin_addr), ntohs(clientsaddr[index].clientaddr.sin_port));
-                write_stdout(msg);
-            }
-            else {
-                write_stdout("Client overflow");
-                continue;
-            }            
-        }
-
-        for (int i = 0; i < client_count; i++) {
-            if (!clients_equal(&recvaddr, &clientsaddr[i].clientaddr)) {
-                if (send_sock(sockfd, &clientsaddr[i].clientaddr, (socklen_t) sizeof(clientsaddr[i].clientaddr)) == -1) {
-                    close(sockfd);
-                    exit(EXIT_FAILURE);
-                }
-            }
-        }
+    if (pthread_create(&procthread, NULL, processing_thread, NULL) != 0) {
+        perror("pthread_create failed");
+        close(sockfd);
+        exit(EXIT_FAILURE);
     }
+
+    pthread_join(procthread, NULL);
 
     close(sockfd);
     exit(EXIT_SUCCESS);
 }
 
-void handle_sigint(int sockfd, struct sockaddr_in *addr) {
+void handle_sigint() {
     running = 0; 
-
-    struct sockaddr_in stopaddr;
-    stopaddr.sin_family = AF_INET;
-    stopaddr.sin_port = addr->sin_port;
-    stopaddr.sin_addr.s_addr = addr->sin_addr.s_addr;
-    sendto(sockfd, "exit", 4, 0, (struct sockaddr *) &stopaddr, (socklen_t) sizeof(stopaddr));
+    pthread_cancel(procthread);
 }
 
 void write_stdout(const char *str) {
@@ -150,10 +122,9 @@ int add_clientaddr(struct socksaddr_in *clientsaddr, struct sockaddr_in *addr) {
     return -1;
 }
 
-int listen_sock(int sockfd, struct sockaddr_in *addr, socklen_t *addrlen) {
-    char bufline[MAX_BUF];   
+int listen_sock(int sockfd, struct sockaddr_in *addr, socklen_t *addrlen, char *bufline) {   
     memset(bufline, 0, MAX_BUF);      
-    if (recvfrom(sockfd, bufline, sizeof(MAX_BUF), 0, (struct sockaddr *) addr, addrlen) == -1) {
+    if (recvfrom(sockfd, bufline, MAX_BUF, 0, (struct sockaddr *) addr, addrlen) == -1) {
         perror("recvfrom failed");
         return -1;
     }
@@ -161,12 +132,50 @@ int listen_sock(int sockfd, struct sockaddr_in *addr, socklen_t *addrlen) {
     return 0;
 }
 
-int send_sock(int sockfd, struct sockaddr_in *addr, socklen_t addrlen) {
-    char bufline[MAX_BUF];   
-    if (sendto(sockfd, bufline, MAX_BUF, 0, (struct sockaddr*) addr, addrlen) == -1) {
+int send_sock(int sockfd, struct sockaddr_in *addr, socklen_t addrlen, char *bufline) {
+    if (sendto(sockfd, bufline, strlen(bufline), 0, (struct sockaddr*) addr, addrlen) == -1) {
         perror("sendto failed");
         return -1;
     }
 
     return 0;
+}
+
+void* processing_thread(void *arg) {
+    char                bufline[MAX_BUF];
+    struct sockaddr_in  recvaddr;
+    socklen_t           recvaddrlen = sizeof(recvaddr);
+
+    /* Process one message at a time */
+    while (running) {
+        pthread_testcancel();
+
+        if (listen_sock(sockfd, &recvaddr, &recvaddrlen, bufline) == -1) {
+            break;
+        }
+
+        if (!is_exists(clientsaddr, &recvaddr) && client_count < CLI_COUNT) {             
+            int index = -1;
+            char msg[MAX_BUF];
+            if ((index = add_clientaddr(clientsaddr, &recvaddr)) != -1) {
+                client_count++;
+                snprintf(msg, sizeof(msg), "New client: %s:%d\n", inet_ntoa(clientsaddr[index].clientaddr.sin_addr), ntohs(clientsaddr[index].clientaddr.sin_port));
+                write_stdout(msg);
+            }
+            else {
+                write_stdout("Client overflow");
+                continue;
+            }            
+        }
+
+        for (int i = 0; i < client_count; i++) {
+            if (!clients_equal(&recvaddr, &clientsaddr[i].clientaddr)) {
+                if (send_sock(sockfd, &clientsaddr[i].clientaddr, (socklen_t) sizeof(clientsaddr[i].clientaddr), bufline) == -1) {
+                    break;
+                }
+            }
+        }
+    }
+
+    return NULL;
 }
